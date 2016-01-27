2720bd61074fde21aa1b44ecd83fcddc8a55f4d0
Rob Brazier <RobBrazier@users.noreply.github.com>
2016-01-27 22:42:05 +0000

    use nodejs

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,24 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	uglify = require('gulp-uglifyjs'),
-	minifyCss = require('gulp-minify-css');
+	uglify = require('gulp-uglify'),
+	minifyCss = require('gulp-minify-css'),
+	del = require('del'),
+	concat = require('gulp-concat');
 
 gulp.task('default', ['sass', 'js']);
 
 gulp.task('sass', function() {
-	gulp.src('scss/style.scss')
+	del('public/assets/css');
+	return gulp.src('scss/style.scss')
 		.pipe(sass())
 		.pipe(minifyCss())
-		.pipe(gulp.dest('assets/css'));
+		.pipe(gulp.dest('public/assets/css'));
 });
 
 gulp.task('js', function() {
-	gulp.src(['js/jquery.min.js', 'js/bootstrap.min.js', 'htmlentities.js', 'scripts.js'])
-		.pipe(uglify('scripts.js'))
-		.pipe(gulp.dest('assets/js'));
+	del('public/assets/js');
+	return gulp.src(['js/jquery.min.js', 'js/bootstrap.min.js', 'js/htmlentities.js', 'js/scripts.js'])
+		.pipe(uglify())
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('public/assets/js'));
 });
