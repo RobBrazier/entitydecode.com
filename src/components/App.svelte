f9dc7d9a7d7483b1c5f10fc6d95299ef64a6c9cd
Rob Brazier <RobBrazier@users.noreply.github.com>
2020-02-27 18:59:03 +0000

    move app to #app to support fathom

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,24 +1,22 @@
-<div>
-  <section class="section">
-    <div class="container">
-      <h2 class="title has-text-centered"><strong>Entity</strong>Decode</h2>
-      <About />
-      <div id="options" class="row has-text-centered">
-        <label class="column">
-          <input type="checkbox" bind:checked={htmlEntity} />
-          HTML Entity Encode/Decode
-        </label>
-        <label class="column" title="Currently only Decoding is supported as Encoding causes too many problems">
-          <input type="checkbox" bind:checked={urlEncoding} />
-          URL Encoding Decode (?)
-        </label>
-      </div>
-      <Field on:data-change={decode} id="encode" bind:data={encoded} label="Encoded" placeholder="e.g. &amp;raquo;" />
-      <Field on:data-change={encode} id="decode" bind:data={decoded} label="Decoded" placeholder="e.g. &raquo;" />
+<section class="section">
+  <div class="container">
+    <h2 class="title has-text-centered"><strong>Entity</strong>Decode</h2>
+    <About />
+    <div id="options" class="row has-text-centered">
+      <label class="column">
+        <input type="checkbox" bind:checked={htmlEntity} />
+        HTML Entity Encode/Decode
+      </label>
+      <label class="column" title="Currently only Decoding is supported as Encoding causes too many problems">
+        <input type="checkbox" bind:checked={urlEncoding} />
+        URL Encoding Decode (?)
+      </label>
     </div>
-  </section>
-  <Footer/>
-</div>
+    <Field on:data-change={decode} id="encode" bind:data={encoded} label="Encoded" placeholder="e.g. &amp;raquo;" />
+    <Field on:data-change={encode} id="decode" bind:data={decoded} label="Decoded" placeholder="e.g. &raquo;" />
+  </div>
+</section>
+<Footer/>
 
 <style global lang="scss">
   @import 'milligram/src/Color';
